398e0a8dbca838d95ee5d42af3064b624074660d
Ludovic Rousseau <ludovic.rousseau@free.fr>
2005-11-24 15:32:41 +0000

    my check if the applet is present was wrong
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@1743 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- examples/scardcontrol.c
+++ examples/scardcontrol.c
@@ -277,7 +277,7 @@ int main(int argc, char *argv[])
 		printf(" %02X", bRecvBuffer[i]);
 	printf("\n");
 	PCSC_ERROR_EXIT(rv, "SCardTransmit")
-	if ((bRecvBuffer[0] != 0x90) || (bRecvBuffer[0] != 0x00))
+	if ((bRecvBuffer[0] != 0x90) || (bRecvBuffer[1] != 0x00))
 	{
 		printf("Error: test applet not found!\n");
 		goto end;
