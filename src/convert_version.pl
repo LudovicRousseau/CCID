a6b39170b6930a60870e3f189ceb73396d834b93
Ludovic Rousseau <ludovic.rousseau@free.fr>
2006-09-20 13:39:23 +0000

    Perl script to convert a version format from "1.2.3-svn-xyz" to "0x01020003"
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@2165 0ce88b0d-b2fd-0310-8134-9614164e65ea

---
+++ src/convert_version.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+#    convert_version.pl: generate a version integer from a version text
+#
+#    Copyright (C) 2006  Ludovic Rousseau  <ludovic.rousseau@free.fr>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+use warnings;
+use strict;
+
+# convert "1.2.3-svn-xyz" in "0x01020003"
+my ($major, $minor, $patch) = split /\./, $ARGV[0];
+
+# remove the -svn-xyz part if any
+$patch =~ s/-.*//;
+
+printf "0x%02X%02X%04X\n", $major, $minor, $patch;
+
