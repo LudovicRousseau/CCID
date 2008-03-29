5f51bfee7f119b8e826ce57a28f1f9bee53c6ab4
Ludovic Rousseau <ludovic.rousseau@free.fr>
2008-03-29 15:12:39 +0000

    use defined(USE_USB_INTERRUPT) instead of just USE_USB_INTERRUPT
    ifdhandler.c:60:66: warning: "USE_USB_INTERRUPT" is not defined
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@2857 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/ifdhandler.c
+++ src/ifdhandler.c
@@ -57,7 +57,7 @@ int PowerOnVoltage = VOLTAGE_5V;
 static int DebugInitialized = FALSE;
 
 /* local functions */
-#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && USE_USB_INTERRUPT
+#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && defined(USE_USB_INTERRUPT)
 static RESPONSECODE IFDHPolling(DWORD Lun);
 static RESPONSECODE IFDHSleep(DWORD Lun);
 #endif
@@ -250,7 +250,7 @@ EXTERNAL RESPONSECODE IFDHCloseChannel(DWORD Lun)
 } /* IFDHCloseChannel */
 
 
-#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && USE_USB_INTERRUPT
+#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && defined(USE_USB_INTERRUPT)
 static RESPONSECODE IFDHPolling(DWORD Lun)
 {
 	int reader_index;
@@ -381,7 +381,7 @@ EXTERNAL RESPONSECODE IFDHGetCapabilities(DWORD Lun, DWORD Tag,
 				*(uint32_t *)Value = get_ccid_descriptor(reader_index) -> dwMaxCCIDMessageLength -10;
 			break;
 
-#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && USE_USB_INTERRUPT
+#if HAVE_DECL_TAG_IFD_POLLING_THREAD && !defined(TWIN_SERIAL) && defined(USE_USB_INTERRUPT)
 		case TAG_IFD_POLLING_THREAD:
 			{
 				_ccid_descriptor *ccid_desc;
