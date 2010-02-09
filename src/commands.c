cb9ce0c481e8fadc430cf5920193449784c638e2
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-02-09 21:29:05 +0000

    Fix 2 compiler warnings
    commands.c: In function ‘CCID_Transmit’:
    commands.c:1107: warning: passing argument 5 of ‘ControlUSB’ discards qualifiers from pointer target type
    ccid_usb.h:43: note: expected ‘unsigned char *’ but argument is of type ‘const unsigned char *’
    commands.c:1130: warning: passing argument 5 of ‘ControlUSB’ discards qualifiers from pointer target type
    ccid_usb.h:43: note: expected ‘unsigned char *’ but argument is of type ‘const unsigned char *’
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@4749 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/commands.c
+++ src/commands.c
@@ -1104,7 +1104,8 @@ RESPONSECODE CCID_Transmit(unsigned int reader_index, unsigned int tx_length,
 		int r;
 
 		/* Xfr Block */
-		r = ControlUSB(reader_index, 0x21, 0x65, 0, tx_buffer, tx_length);
+		r = ControlUSB(reader_index, 0x21, 0x65, 0,
+			(unsigned char *)tx_buffer, tx_length);
 
 		/* we got an error? */
 		if (r < 0)
@@ -1126,8 +1127,8 @@ RESPONSECODE CCID_Transmit(unsigned int reader_index, unsigned int tx_length,
 
 		/* Xfr Block */
 		DEBUG_COMM2("chain parameter: %d", rx_length);
-		r = ControlUSB(reader_index, 0x21, 0x65, rx_length << 8, tx_buffer,
-			tx_length);
+		r = ControlUSB(reader_index, 0x21, 0x65, rx_length << 8,
+			(unsigned char *)tx_buffer, tx_length);
 
 		/* we got an error? */
 		if (r < 0)
