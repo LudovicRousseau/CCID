ca9bbfebc5b8626bed3c5f2cabb2119a00134d89
Ludovic Rousseau <ludovic.rousseau@free.fr>
2010-06-18 19:05:32 +0000

    use libusb_detach_kernel_driver() from libusb-1.0 instead of the *_np() version
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@5015 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- contrib/Kobil_mIDentity_switch/Kobil_mIDentity_switch.c
+++ contrib/Kobil_mIDentity_switch/Kobil_mIDentity_switch.c
@@ -116,9 +116,8 @@ static int kobil_midentity_claim_interface(libusb_device_handle *usb, int ifnum)
 		return rv;
 	}
 
-#ifdef HAVE_USB_DETACH_KERNEL_DRIVER_NP
 	printf("failed with error %d, trying to detach kernel driver ....\n", rv);
-	rv = libusb_detach_kernel_driver_np(usb, ifnum);
+	rv = libusb_detach_kernel_driver(usb, ifnum);
 	if (rv == 0)
 	{
 		printf("success, claiming interface again ...");
@@ -129,7 +128,7 @@ static int kobil_midentity_claim_interface(libusb_device_handle *usb, int ifnum)
 			return rv;
 		}
 	}
-#endif
+
 	printf("failed with error %d, giving up.\n", rv);
 	return rv;
 }
