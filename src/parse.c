409b22da4d57656b142a7737667e90344731b1fc
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-01-04 09:46:46 +0000

    Define log level to maximum to get error messages from ccid_usb.c
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@5472 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/parse.c
+++ src/parse.c
@@ -48,7 +48,7 @@
 #define NORMAL "\33[0m"
 
 /* global variables used in ccid_usb.c but defined in ifdhandler.c */
-int LogLevel = 0;
+int LogLevel = 1+2+4+8; /* full debug */
 int DriverOptions = 0;
 
 static int ccid_parse_interface_descriptor(libusb_device_handle *handle,
