c8677653f46686035c0e7b5d6d81ce63f4096467
Ludovic Rousseau <ludovic.rousseau@free.fr>
2011-01-07 14:45:50 +0000

    CloseUSB(): free the serial number string only for the last slot since they all point to the same string.
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@5494 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/ccid_usb.c
+++ src/ccid_usb.c
@@ -714,6 +714,9 @@ status_t CloseUSB(unsigned int reader_index)
 	{
 		DEBUG_COMM("Last slot closed. Release resources");
 
+		if (usbDevice[reader_index].ccid.sIFD_serial_number)
+			free(usbDevice[reader_index].ccid.sIFD_serial_number);
+
 		/* reset so that bSeq starts at 0 again */
 		if (DriverOptions & DRIVER_OPTION_RESET_ON_CLOSE)
 			(void)libusb_reset_device(usbDevice[reader_index].dev_handle);
@@ -729,9 +732,6 @@ status_t CloseUSB(unsigned int reader_index)
 	/* mark the resource unused */
 	usbDevice[reader_index].dev_handle = NULL;
 	usbDevice[reader_index].interface = 0;
-
-	if (usbDevice[reader_index].ccid.sIFD_serial_number)
-		free(usbDevice[reader_index].ccid.sIFD_serial_number);
 
 	return STATUS_SUCCESS;
 } /* CloseUSB */
