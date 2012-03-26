0013da5941c5883b483db3bb313fadd175c4e7b5
Ludovic Rousseau <ludovic.rousseau@free.fr>
2012-03-26 14:11:14 +0000

    SecurePINModify(): set the minimum timeout to 90 seconds
    Some readers have a default timeout of 60 seconds. We need to use a
    larger value to timeout _after_ the reader.
    
    Same change as in revision 6243 but for SecurePINModify() instead of
    SecurePINVerify()
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@6251 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/commands.c
+++ src/commands.c
@@ -702,7 +702,7 @@ RESPONSECODE SecurePINModify(unsigned int reader_index,
 	i2dw(a - 10, cmd + 1);	/* command length (includes bPINOperation) */
 
 	old_read_timeout = ccid_descriptor -> readTimeout;
-	ccid_descriptor -> readTimeout = max(30, TxBuffer[0]+10)*1000;	/* at least 30 seconds */
+	ccid_descriptor -> readTimeout = max(90, TxBuffer[0]+10)*1000;	/* at least 90 seconds */
 
 	if (WritePort(reader_index, a, cmd) != STATUS_SUCCESS)
 	{
