c1ffbc263cd6d2ab328d1ed78976d45bd2e3dd08
Ludovic Rousseau <ludovic.rousseau@free.fr>
2013-11-25 13:09:41 +0000

    Generalize the management of readers with bDeviceClass = 0xFF
    Some early Gemalto Ezio CB+ readers have bDeviceClass, bDeviceSubClass and
    bDeviceProtocol set to 0xFF (proprietary) instead of 0x00.
    
    So on Mac OS X the reader configuration is not done by the OS/kernel and we do
    it ourself.
    
    The code is no more limited to the GEMALTO_EZIO_CBP (0x08E6/0x34C3) but to any
    reader with the problem, like the Todos Argos Mini II CCID.
    
    
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/Drivers/ccid@6793 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- src/ccid_usb.c
+++ src/ccid_usb.c
@@ -497,35 +497,45 @@ status_t OpenUSBByName(unsigned int reader_index, /*@null@*/ char *device)
 				}
 
 again:
-#ifdef __APPLE__
-				/* Some early Gemalto Ezio CB+ readers have
-				 * bDeviceClass, bDeviceSubClass and bDeviceProtocol set
-				 * to 0xFF (proprietary) instead of 0x00.
-				 *
-				 * So on Mac OS X the reader configuration is not done
-				 * by the OS/kernel and we do it ourself.
-				 */
-				if (GEMALTO_EZIO_CBP == readerID)
-				{
-					r = libusb_set_configuration(dev_handle, 1);
-					if (r < 0)
-					{
-						(void)libusb_close(dev_handle);
-						DEBUG_CRITICAL4("Can't set configuration on %d/%d: %d",
-							bus_number, device_address, r);
-						continue;
-					}
-				}
-#endif
-
 				r = libusb_get_active_config_descriptor(dev, &config_desc);
 				if (r < 0)
 				{
-					(void)libusb_close(dev_handle);
-					DEBUG_CRITICAL4("Can't get config descriptor on %d/%d: %d",
-						bus_number, device_address, r);
-					continue;
+#ifdef __APPLE__
+					/* Some early Gemalto Ezio CB+ readers have
+					 * bDeviceClass, bDeviceSubClass and bDeviceProtocol set
+					 * to 0xFF (proprietary) instead of 0x00.
+					 *
+					 * So on Mac OS X the reader configuration is not done
+					 * by the OS/kernel and we do it ourself.
+					 */
+					if ((0xFF == desc.bDeviceClass)
+						&& (0xFF == desc.bDeviceSubClass)
+						&& (0xFF == desc.bDeviceProtocol))
+					{
+						r = libusb_set_configuration(dev_handle, 1);
+						if (r < 0)
+						{
+							(void)libusb_close(dev_handle);
+							DEBUG_CRITICAL4("Can't set configuration on %d/%d: %d",
+									bus_number, device_address, r);
+							continue;
+						}
+					}
+
+					/* recall */
+					r = libusb_get_active_config_descriptor(dev, &config_desc);
+					if (r < 0)
+					{
+#endif
+						(void)libusb_close(dev_handle);
+						DEBUG_CRITICAL4("Can't get config descriptor on %d/%d: %d",
+							bus_number, device_address, r);
+						continue;
+					}
+#ifdef __APPLE__
 				}
+#endif
+
 
 				usb_interface = get_ccid_usb_interface(config_desc, &num);
 				if (usb_interface == NULL)
