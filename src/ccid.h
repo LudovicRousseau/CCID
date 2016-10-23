740452fd780cf59798e9eb5b45c0f62f85a47707
Nick Kossifidis <mickflemm@gmail.com>
2016-10-23 13:54:13 -0400

    Forgot to add the id for vmware ccid

--- src/ccid.h
+++ src/ccid.h
@@ -213,6 +213,7 @@ typedef struct
 #define ElatecTWN4	0x09D80427
 #define SCM_SCL011 0x04E65293
 #define HID_AVIATOR	0x076B3A21
+#define	VMWARE_CCID	0x0E0F0004
 
 #define VENDOR_GEMALTO 0x08E6
 #define GET_VENDOR(readerID) ((readerID >> 16) & 0xFFFF)
