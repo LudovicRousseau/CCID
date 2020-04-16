ce899b991d5ee7857eb337ef4947ab30f280d3eb
Ludovic Rousseau <ludovic.rousseau@free.fr>
2020-04-16 13:52:05 +0200

    Revert back to .device_address instead of .port_number
    The port number is hard to use. For example when you use a USB hub you
    can have 2 devices on port 1.
    The real solution with port is to use libusb_get_port_numbers() to get a
    list of ports. But we can only return one byte with SCARD_ATTR_CHANNEL_ID.
    
    The device address is easy to use. If you know the bus number and
    device address you can uniquely identify a USB device on GNU/Linux.
    
    Example:
    With SCardGetAttrib.py I get:
    0x20110 [8, 1, 32, 0] 08 01 20 00 .. .
     USB: bus: 1, addr: 8
    
    $ lsusb -s 1:8
    Bus 001 Device 008: ID 08e6:3437 Gemalto (was Gemplus) GemPC Twin SmartCard Reader

--- src/ccid_usb.c
+++ src/ccid_usb.c
@@ -81,7 +81,6 @@ typedef struct
 	uint8_t bus_number;
 	uint8_t device_address;
 	int interface;
-	uint8_t port_number;
 
 	/*
 	 * Endpoints
@@ -698,7 +697,6 @@ again:
 				usbDevice[reader_index].real_nb_opened_slots = 1;
 				usbDevice[reader_index].nb_opened_slots = &usbDevice[reader_index].real_nb_opened_slots;
 				usbDevice[reader_index].polling_transfer = NULL;
-				usbDevice[reader_index].port_number = libusb_get_port_number(dev);
 
 				/* CCID common informations */
 				usbDevice[reader_index].ccid.real_bSeq = 0;
@@ -1114,12 +1112,12 @@ uint8_t get_ccid_usb_bus_number(int reader_index)
 
 /*****************************************************************************
  *
- *					get_ccid_usb_port_number
+ *					get_ccid_usb_device_address
  *
  ****************************************************************************/
-uint8_t get_ccid_usb_port_number(int reader_index)
+uint8_t get_ccid_usb_device_address(int reader_index)
 {
-	return usbDevice[reader_index].port_number;
+	return usbDevice[reader_index].device_address;
 }
 
 /*****************************************************************************
