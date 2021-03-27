LudovicRousseau/CCID

commit e8216d4f666582c7c299755d139247b7758f2646

Solve a performance issue with T=1 and CCID_CLASS_AUTO_PPS_PROP
Set IFSC in all cases so that the driver uses the value returned in the
card ATR.
The driver will be able to send data in bigger chunks up to the card
IFSC value instead of the default value of 32 bytes.

Thanks to Florent VIBERT for the patch

src/ifdhandler.c
@@ -723,14 +723,6 @@ EXTERNAL RESPONSECODE IFDHSetProtocolParameters(DWORD Lun, DWORD Protocol,
 	ccid_slot = get_ccid_slot(reader_index);
 	ccid_desc = get_ccid_descriptor(reader_index);
 
-	/* Do not send CCID command SetParameters or PPS to the CCID
-	 * The CCID will do this himself */
-	if (ccid_desc->dwFeatures & CCID_CLASS_AUTO_PPS_PROP)
-	{
-		DEBUG_COMM2("Timeout: %d ms", ccid_desc->readTimeout);
-		goto end;
-	}
-
 	/* check the protocol is supported by the reader */
 	if (!(Protocol & ccid_desc->dwProtocols))
 	{
@@ -794,6 +786,14 @@ EXTERNAL RESPONSECODE IFDHSetProtocolParameters(DWORD Lun, DWORD Protocol,
 				/* only the first TCi (i>2) must be used */
 				break;
 			}
+	}
+
+    /* Do not send CCID command SetParameters or PPS to the CCID
+	 * The CCID will do this himself */
+	if (ccid_desc->dwFeatures & CCID_CLASS_AUTO_PPS_PROP)
+	{
+		DEBUG_COMM2("Timeout: %d ms", ccid_desc->readTimeout);
+		goto end;
 	}
 
 	/* PTS1? */
@@ -1092,6 +1092,7 @@ EXTERNAL RESPONSECODE IFDHSetProtocolParameters(DWORD Lun, DWORD Protocol,
 			return ret;
 	}
 
+end:
 	/* set IFSC & IFSD in T=1 */
 	if (SCARD_PROTOCOL_T1 == Protocol)
 	{
@@ -1117,7 +1118,6 @@ EXTERNAL RESPONSECODE IFDHSetProtocolParameters(DWORD Lun, DWORD Protocol,
 		DEBUG_COMM3("T=1: IFSC=%d, IFSD=%d", t1->ifsc, t1->ifsd);
 	}
 
-end:
 	/* store used protocol for use by the secure commands (verify/change PIN) */
 	ccid_desc->cardProtocol = Protocol;
 

commit 05546f902e387ee5ab49799962c818a1773ad548

IFDHSetProtocolParameters: set IFSC/IFSD only for TPDU readers
Sending a T=1 block to an APDU reader is a bad idea. The reader will
interpret the T=1 data block as an APDU and it will fail.

This commit fixes a problem introduced by
e8216d4f666582c7c299755d139247b7758f2646 (not released in a stable
version)

src/ifdhandler.c
@@ -1094,7 +1094,8 @@ EXTERNAL RESPONSECODE IFDHSetProtocolParameters(DWORD Lun, DWORD Protocol,
 
 end:
 	/* set IFSC & IFSD in T=1 */
-	if (SCARD_PROTOCOL_T1 == Protocol)
+	if ((SCARD_PROTOCOL_T1 == Protocol)
+		&& (CCID_CLASS_TPDU == (ccid_desc->dwFeatures & CCID_CLASS_EXCHANGE_MASK)))
 	{
 		t1_state_t *t1 = &(ccid_slot -> t1);
 		int i, ifsc;