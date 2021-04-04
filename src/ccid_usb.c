LudovicRousseau/CCID

commit b00292f718e9f13626147af63bfb9164ec74d2cb

ccid_usb: ask for bNumDataRatesSupported data rates
Some devices set bNumDataRatesSupported to a value different from 0 but
libusb_control_transfer() fails if another value (like 256) is used.

The Alcor Micro 9540 is such a reader.

src/ccid_usb.c
@@ -1210,13 +1210,21 @@ static unsigned int *get_data_rates(unsigned int reader_index,
 	int n, i, len;
 	unsigned char buffer[256*sizeof(int)];	/* maximum is 256 records */
 	unsigned int *uint_array;
+	int bNumDataRatesSupported;
+
+	bNumDataRatesSupported = get_ccid_device_descriptor(get_ccid_usb_interface(desc, &num))[27];
+	if (0 == bNumDataRatesSupported)
+		/* read up to the buffer size */
+		len = sizeof(buffer) / sizeof(int);
+	else
+		bNumDataRatesSupported = len;
 
 	/* See CCID 3.7.3 page 25 */
 	n = ControlUSB(reader_index,
 		0xA1, /* request type */
 		0x03, /* GET_DATA_RATES */
 		0x00, /* value */
-		buffer, sizeof(buffer));
+		buffer, len * sizeof(int));
 
 	/* we got an error? */
 	if (n <= 0)
@@ -1236,8 +1244,7 @@ static unsigned int *get_data_rates(unsigned int reader_index,
 	n /= sizeof(int);
 
 	/* we do not get the expected number of data rates */
-	len = get_ccid_device_descriptor(get_ccid_usb_interface(desc, &num))[27]; /* bNumDataRatesSupported */
-	if ((n != len) && len)
+	if ((n != bNumDataRatesSupported) && bNumDataRatesSupported)
 	{
 		DEBUG_INFO3("Got %d data rates but was expecting %d", n, len);
 

commit 19b9e9256781b55513e22e8ecd9c85ad409e9069

get_data_rates(): fix bug if bNumDataRatesSupported > 0
The bug was introduced in b00292f718e9f13626147af63bfb9164ec74d2cb (not
released in a stable version)

Thanks to Godfrey Chung got the bug report
https://github.com/LudovicRousseau/CCID/commit/b00292f718e9f13626147af63bfb9164ec74d2cb#r49087277

src/ccid_usb.c
@@ -1217,7 +1217,7 @@ static unsigned int *get_data_rates(unsigned int reader_index,
 		/* read up to the buffer size */
 		len = sizeof(buffer) / sizeof(int);
 	else
-		bNumDataRatesSupported = len;
+		len = bNumDataRatesSupported;
 
 	/* See CCID 3.7.3 page 25 */
 	n = ControlUSB(reader_index,