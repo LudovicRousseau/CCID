LudovicRousseau/CCID

commit a382095d8b19730c92969052f961a2066bc04c0f

CmdPowerOn: fix a potention overrun with bogus ATR
If the ATR is larger than 33 bytes (MAX_ATR_SIZE) then we had a buffer
overrun.
This should never happen with correcr cards and readers.

Thanks to Maksim Ivanov for the bug report
"[Pcsclite-muscle] Insufficient checks in CCID"
http://lists.infradead.org/pipermail/pcsclite-muscle/2020-August/001098.html

" Hello,

The CCID free software driver is missing a few checks and graceful
handling of some error cases:

2. Wrong (too big) ATR size received from the device. (Would lead to a
buffer overrun in CmdPowerOn() when doing memmove() in the buffer:
https://salsa.debian.org/rousseau/CCID/-/blob/4d5cbf703c268b31c734931166c52dcb9920c0fe/src/commands.c#L294
"

src/commands.c
@@ -289,11 +289,12 @@ again:
 
 	/* extract the ATR */
 	atr_len = dw2i(buffer, 1);	/* ATR length */
-	if (atr_len > *nlength)
-		atr_len = *nlength;
+	if (atr_len > *nlength - 10)
+		atr_len = *nlength - 10;
 	else
 		*nlength = atr_len;
 
+	/* the buffer length should be 10 + MAX_ATR_SIZE */
 	memmove(buffer, buffer+10, atr_len);
 
 	return return_value;

commit 2685083589ef9b97796b9e80092910859e27d777

commands: CmdPowerOn: Fix nlength return value in overflow case
CmdPowerOn is supposed to return ATR and its length in
buffer/nlength.

A bounds check is performed in the code, and truncates atr_len
to the length of the received data (minus 10 bytes for the header).
However, in that case, nlength in not updated, leading to a returned
ATR with trailing garbage.

This is an edge, error case, that should never happen, and was
introduced while fixing a potentially more serious buffer overrun
in 2020.

Fixes: a382095d8b19 ("CmdPowerOn: fix a potention overrun with bogus ATR")

src/commands.c
@@ -294,8 +294,8 @@ again:
 	atr_len = dw2i(buffer, 1);	/* ATR length */
 	if (atr_len > *nlength - 10)
 		atr_len = *nlength - 10;
-	else
-		*nlength = atr_len;
+
+	*nlength = atr_len;
 
 	/* the buffer length should be 10 + MAX_ATR_SIZE */
 	memmove(buffer, buffer+10, atr_len);