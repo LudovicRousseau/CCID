ab307eaaed9f5e6a90728a433750e44b46340eef
Ludovic Rousseau <ludovic.rousseau@free.fr>
2024-10-24 18:30:17 +0200

    Fix compiler warning: variable may be used uninitialized
    ../src/commands.c: In function ‘CmdEscapeCheck’:
    ../src/commands.c:1057:48: warning: ‘old_read_timeout’ may be used uninitialized [-Wmaybe-uninitialized]
     1057 |                 ccid_descriptor -> readTimeout = old_read_timeout;
          |                                                ^
    ../src/commands.c:948:13: note: ‘old_read_timeout’ was declared here
      948 |         int old_read_timeout;
          |             ^

--- src/commands.c
+++ src/commands.c
@@ -945,7 +945,7 @@ RESPONSECODE CmdEscapeCheck(unsigned int reader_index,
 	status_t res;
 	unsigned int length_in, length_out;
 	RESPONSECODE return_value = IFD_SUCCESS;
-	int old_read_timeout;
+	int old_read_timeout = -1;
 	_ccid_descriptor *ccid_descriptor = get_ccid_descriptor(reader_index);
 
 	/* a value of 0 do not change the default read timeout */
